aec8f9678f421256b9e2813c3fb0fa69715bf4ad
OXIDE <oxidiu777@gmail.com>
2023-05-31 14:56:58 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import "./App.css";
 import CardComponent from "./CardComponent.jsx";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const []
+
   return (
     <div>
+      <input/>
+
       <CardComponent
         content={
           "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam."
